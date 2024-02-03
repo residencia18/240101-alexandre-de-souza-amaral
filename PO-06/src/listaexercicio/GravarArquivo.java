5ea1a68c44a1ee7d4276cc3ef8b58a0570cd29e1
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2024-02-03 11:21:32 -0300

    feita primeira parte atividade 07

--- PO-06/src/listaexercicio/GravarArquivo.java
+++ PO-06/src/listaexercicio/GravarArquivo.java
@@ -1,5 +1,34 @@
 package listaexercicio;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
+
 public class GravarArquivo {
 
+    public static void main(String[] args) {
+        String nomeArquivo = "saida.txt";
+
+        try {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Digite as linhas de texto (digite 'fim' para encerrar):");
+
+            while (true) {
+                String linha = scanner.nextLine();
+                if (linha.equalsIgnoreCase("fim")) {
+                    break;
+                }
+
+                Files.writeString(Paths.get(nomeArquivo), linha + System.lineSeparator(),
+                        StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+            }
+
+            System.out.println("As linhas foram gravadas no arquivo '" + nomeArquivo + "'.");
+        } catch (IOException e) {
+            System.err.println("Erro ao gravar no arquivo: " + e.getMessage());
+        }
+    }
 }
--- filmes/src/filmes/Filme.java
+++ filmes/src/filmes/Filme.java
@@ -1,6 +1,6 @@
 package filmes;
 
-import org.json.JSONArray;
+import org.json.JSONArray;	
 import org.json.JSONObject;
 
 public class Filme {
